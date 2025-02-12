00fb254a1ff13419292f418d9459bbb98e094ed6
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-02-12 15:06:42 +0300

    added setAccount and getAccount for store

--- src/stores/accounts.ts
+++ src/stores/accounts.ts
@@ -1,9 +1,18 @@
 import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 import type { AccountsType } from '../types/accounts';
-
 export const useAccountsStore = defineStore('accounts', () => {
   let accounts: Ref<AccountsType[] | null> = ref(null);
+  function setAccounts(item: AccountsType[]) {
+   accounts.value =  [...item];
+   localStorage.setItem("accounts", JSON.stringify(accounts.value));
+  }
+  function getAccountsFromLocal() {
+    let localAccounts = JSON.parse(localStorage.getItem('accounts')!);
+    if (localAccounts) {
+      accounts.value = [...localAccounts]
+    }
+  }
 
-  return { accounts }
+  return { accounts, getAccountsFromLocal, setAccounts}
 })
