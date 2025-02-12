207171cf84c7e1e073091b348abc48253a63449c
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-02-12 14:23:06 +0300

    delete components added icons

---
+++ src/components/icons/IconHidePassword.vue
@@ -0,0 +1,19 @@
+<template>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="16"
+  height="16"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="#000"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M17.94 17.94A10.93 10.93 0 0 1 12 20C5 20 1 12 1 12a17.91 17.91 0 0 1 3.06-4.94"/>
+  <path d="M9.88 9.88A3 3 0 0 0 12 15a3 3 0 0 0 2.12-5.12"/>
+  <path d="M22 12s-4-8-11-8a10.84 10.84 0 0 0-4.58 1"/>
+  <line x1="2" y1="2" x2="22" y2="22"/>
+</svg>
+
+</template>
